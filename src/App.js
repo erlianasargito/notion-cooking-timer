cc1a7fe63a891248fed381593cd3fd64feccfcd4
Erliana <erlianasrgt@gmail.com>
2023-02-13 12:41:21 +0700

    Section Boil Egg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Card, Container, Col, Button } from 'react-bootstrap';
+import egg from '../src/assets/img/eggs.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className='p-4'>  
+  <Col md="4">  
+  <Card>  
+  <Card.Img variant="top" src={egg} />  
+  <Card.Body>  
+    <Card.Title>Card Title</Card.Title>  
+    <Card.Text>  
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae molestie magna. Vivamus sed molestie enim, eu convallis mauris. Aliquam pharetra velit ac enim maximus, a commodo augue hendrerit. Phasellus at aliquam est  
+    </Card.Text>  
+    <Button variant="primary">Read More</Button>  
+  </Card.Body>  
+</Card>  
+    </Col>  
+</Container>  
     </div>
   );
 }
